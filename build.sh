b741b928b3afdff55ad9ed4ca4f990b7beb3b2ab
Pachimarik <sahok_v@mail.ru>
2022-03-24 16:09:24 +0000

    mmmm hueta

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ export $(cat  variables | xargs)
 
 sudo amazon-linux-extras install ansible2 -y
 ansible-galaxy collection install community.docker
-sudo amazon-linux-extras install python3.8 -y
-sudo python3.8 -m pip install --upgrade setuptools
-sudo python3.8 -m pip install --upgrade pip
-sudo python3.8 -m pip install ansible
+# sudo amazon-linux-extras install python3.8 -y
+# sudo python3.8 -m pip install --upgrade setuptools
+# sudo python3.8 -m pip install --upgrade pip
+# sudo python3.8 -m pip install ansible
+sudo python2.7 -m pip install docker==4.4.4 --ignore-installed
 
 cd ansible/
 ansible-config view
@@ -16,3 +17,5 @@ ansible -m ping all
 
 ansible-playbook playbook.yml
 ansible-playbook node1.yml
+ansible-playbook node2.yml
+ansible-playbook node3.yml
