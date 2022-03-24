9b3ddaf3fe4ef25f4d7635da34d67ecccd963de0
Pachimarik <sahok_v@mail.ru>
2022-03-24 13:54:25 +0000

    finaly docker is work

--- build.sh
+++ build.sh
@@ -3,11 +3,15 @@ export $(cat  variables | xargs)
 
 #write your ci/cd code here
 
-sudo amazon-linux-extras install ansible2
+sudo amazon-linux-extras install ansible2 -y
+ansible-galaxy collection install community.docker
+sudo amazon-linux-extras install python3.8 -y
+sudo python3.8 -m pip install --upgrade setuptools
+sudo python3.8 -m pip install --upgrade pip
+sudo python3.8 -m pip install ansible
 
 cd ansible/
 ansible-config view
 ansible -m ping all
 
 ansible-playbook playbook.yml
-
