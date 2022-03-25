4f464c622e01e036fee6ff33ed62bf1c8602c535
Pachimarik <sahok_v@mail.ru>
2022-03-25 08:04:47 +0000

    the web site is down

--- build.sh
+++ build.sh
@@ -5,11 +5,9 @@ export $(cat  variables | xargs)
 
 sudo amazon-linux-extras install ansible2 -y
 ansible-galaxy collection install community.docker
-# sudo amazon-linux-extras install python3.8 -y
-# sudo python3.8 -m pip install --upgrade setuptools
-# sudo python3.8 -m pip install --upgrade pip
-# sudo python3.8 -m pip install ansible
 sudo python2.7 -m pip install docker==4.4.4 --ignore-installed
+sudo python2.7 -m pip install --upgrade setuptools
+sudo python2.7 -m pip install --upgrade pip
 
 cd ansible/
 ansible-config view
