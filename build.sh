7e4dab8ead3b11c50a05e15c02e56169feeead9e
Pachimarik <sahok_v@mail.ru>
2022-03-29 11:29:06 +0000

    add replication

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ export $(cat  variables | xargs)
 sudo amazon-linux-extras install ansible2 -y
 ansible-galaxy collection install community.docker
 ansible-galaxy collection install gluster.gluster
+sudo yum install python-pip -y
 sudo python2.7 -m pip install docker==4.4.4 --ignore-installed
 sudo python2.7 -m pip install --upgrade setuptools
 sudo python2.7 -m pip install --upgrade pip
@@ -18,4 +19,3 @@ ansible-playbook playbook.yml
 ansible-playbook node1.yml
 ansible-playbook node2.yml
 ansible-playbook node3.yml
-ansible-playbook datacluster.yml
